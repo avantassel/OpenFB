23c197e1eb96261b58bce535ee1e6a59dbbb234c
Andrew Van Tassel <avantassel@gmail.com>
2014-11-22 14:48:26 -0700

    Update openfb.js
    Added deviceready back

--- openfb.js
+++ openfb.js
@@ -38,6 +38,10 @@ var openFB = (function () {
     //console.log(oauthRedirectURL);
     //console.log(logoutRedirectURL);
 
+    document.addEventListener("deviceready", function () {
+            runningInCordova = true;
+        }, false);
+        
     /**
      * Initialize the OpenFB module. You must use this function and initialize the module with an appId before you can
      * use any other function.
